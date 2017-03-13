2e960318d1e3450935446b0827f8bf74b9a70cf4
crisenchou <269298762@qq.com>
2017-03-13 12:31:22 +0000

    asset

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -7,31 +7,36 @@
 
     <meta name="description" content="User login page"/>
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0"/>
-
     <!-- bootstrap & fontawesome -->
-    <link rel="stylesheet" href="assets/css/bootstrap.min.css"/>
-    <link rel="stylesheet" href="assets/font-awesome/4.5.0/css/font-awesome.min.css"/>
+    <link rel="stylesheet" href="{{asset('assets/css/bootstrap.min.css')}}"/>
+    <link rel="stylesheet" href="{{asset('assets/font-awesome/4.5.0/css/font-awesome.min.css')}}"/>
 
-    <!-- text fonts -->
-    <link rel="stylesheet" href="assets/css/fonts.googleapis.com.css"/>
+    <!-- page specific plugin styles -->
+@stack('css')
+
+<!-- text fonts -->
+    <link rel="stylesheet" href="{{asset('assets/css/fonts.googleapis.com.css')}}"/>
 
     <!-- ace styles -->
-    <link rel="stylesheet" href="assets/css/ace.min.css"/>
+    <link rel="stylesheet" href="{{asset('assets/css/ace.min.css')}}" class="ace-main-stylesheet" id="main-ace-style"/>
 
     <!--[if lte IE 9]>
-    <link rel="stylesheet" href="assets/css/ace-part2.min.css"/>
+    <link rel="stylesheet" href=" {{asset('assets/css/ace-part2.min.css')}}" class="ace-main-stylesheet"/>
     <![endif]-->
-    <link rel="stylesheet" href="assets/css/ace-rtl.min.css"/>
-
     <!--[if lte IE 9]>
-    <link rel="stylesheet" href="assets/css/ace-ie.min.css"/>
+    <link rel="stylesheet" href="{{asset('assets/css/ace-ie.min.css')}}"/>
     <![endif]-->
+
+    <!-- inline styles related to this page -->
+
+    <!-- ace settings handler -->
+    <script src="{{asset('assets/js/ace-extra.min.js')}}"></script>
 
     <!-- HTML5shiv and Respond.js for IE8 to support HTML5 elements and media queries -->
 
     <!--[if lte IE 8]>
-    <script src="assets/js/html5shiv.min.js"></script>
-    <script src="assets/js/respond.min.js"></script>
+    <script src="{{asset('assets/js/html5shiv.min.js')}}"></script>
+    <script src="{{asset('assets/js/respond.min.js')}}"></script>
     <![endif]-->
 </head>
 
@@ -75,14 +80,11 @@
 </div><!-- /.main-container -->
 
 <!-- basic scripts -->
-
 <!--[if !IE]> -->
-<script src="assets/js/jquery-2.1.4.min.js"></script>
-
+<script src="{{asset('assets/js/jquery-2.1.4.min.js')}}"></script>
 <!-- <![endif]-->
-
 <!--[if IE]>
-<script src="assets/js/jquery-1.11.3.min.js"></script>
+<script src="{{asset('assets/js/jquery-1.11.3.min.js')}}"></script>
 <![endif]-->
 <script type="text/javascript">
     if ('ontouchstart' in document.documentElement) document.write("<script src='assets/js/jquery.mobile.custom.min.js'>" + "<" + "/script>");
