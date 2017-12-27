b219f7268d765fddd096a139ff607b4d09eca531
crisenchou <crisen@crisen.org>
2017-12-27 09:35:57 +0000

    tidy struture

---
+++ resources/views/components/index/flot.blade.php
@@ -0,0 +1,26 @@
+@push('css')
+@endpush
+<div class="col-sm-7">
+    <div class="widget-box transparent">
+        <div class="widget-header widget-header-flat">
+            <h4 class="widget-title lighter">
+                <i class="ace-icon fa fa-signal"></i>
+                Sale Stats
+            </h4>
+
+            <div class="widget-toolbar">
+                <a href="#" data-action="collapse">
+                    <i class="ace-icon fa fa-chevron-up"></i>
+                </a>
+            </div>
+        </div>
+
+        <div class="widget-body">
+            <div class="widget-main padding-4">
+                <div id="sales-charts"></div>
+            </div><!-- /.widget-main -->
+        </div><!-- /.widget-body -->
+    </div><!-- /.widget-box -->
+</div><!-- /.col -->
+@push('scripts')
+@endpush
